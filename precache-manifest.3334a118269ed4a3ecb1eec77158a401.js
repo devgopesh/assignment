b4089ad1b42208bbe98dcf12df6c32a3ef759ebc
gopesh301 <gopeshsinghal301@gmail.com>
2018-11-01 14:32:57 +0530

    Updates

---
+++ precache-manifest.3334a118269ed4a3ecb1eec77158a401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/assignment/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b6fb593b5ae9b570acc1",
+    "url": "/assignment/static/js/runtime~main.b6fb593b.js"
+  },
+  {
+    "revision": "a39bbdd712478da6820a",
+    "url": "/assignment/static/js/main.a39bbdd7.chunk.js"
+  },
+  {
+    "revision": "b8c84bdfcf2234cddbb9",
+    "url": "/assignment/static/js/1.b8c84bdf.chunk.js"
+  },
+  {
+    "revision": "a39bbdd712478da6820a",
+    "url": "/assignment/static/css/main.087e68d6.chunk.css"
+  },
+  {
+    "revision": "4348a748334a49398d0f62dd4dc8d4a9",
+    "url": "/assignment/index.html"
+  }
+];

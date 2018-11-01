13d0b98f311f459296a0b1d9425526ea9996db96
gopesh301 <gopeshsinghal301@gmail.com>
2018-11-01 14:39:27 +0530

    file changes

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -6,10 +6,10 @@ const Header = () => {
 		<div>
 			<nav>
 				<div className="nav-wrapper">
-				 <NavLink to='/' className="brand-logo">Profile</NavLink>
+				 <NavLink to='/assignment' className="brand-logo">Profile</NavLink>
 				  <ul id="nav-mobile" className="right hide-on-med-and-down">
-				    <li><NavLink to='/posts'>Posts</NavLink></li>
-				    <li><NavLink to='/messages'>Messages</NavLink></li>				
+				    <li><NavLink to='/assignment/posts'>Posts</NavLink></li>
+				    <li><NavLink to='/assignment/messages'>Messages</NavLink></li>				
 				  </ul>
 				</div>
 			</nav>
